0b376d094326d2384b216cf57d5e0164d308ba3a
Kurt Alfred Kluever <kak@google.com>
2022-06-24 17:57:35 -0700

    Internal change
    PiperOrigin-RevId: 457128604

--- nativetemplates/src/main/java/com/google/android/ads/nativetemplates/NativeTemplateStyle.java
+++ nativetemplates/src/main/java/com/google/android/ads/nativetemplates/NativeTemplateStyle.java
@@ -16,6 +16,7 @@ package com.google.android.ads.nativetemplates;
 
 import android.graphics.Typeface;
 import android.graphics.drawable.ColorDrawable;
+import com.google.errorprone.annotations.CanIgnoreReturnValue;
 
 /** A class containing the optional styling options for the Native Template. * */
 public class NativeTemplateStyle {
@@ -157,86 +158,103 @@ public class NativeTemplateStyle {
       this.styles = new NativeTemplateStyle();
     }
 
+    @CanIgnoreReturnValue
     public Builder withCallToActionTextTypeface(Typeface callToActionTextTypeface) {
       this.styles.callToActionTextTypeface = callToActionTextTypeface;
       return this;
     }
 
+    @CanIgnoreReturnValue
     public Builder withCallToActionTextSize(float callToActionTextSize) {
       this.styles.callToActionTextSize = callToActionTextSize;
       return this;
     }
 
+    @CanIgnoreReturnValue
     public Builder withCallToActionTypefaceColor(int callToActionTypefaceColor) {
       this.styles.callToActionTypefaceColor = callToActionTypefaceColor;
       return this;
     }
 
+    @CanIgnoreReturnValue
     public Builder withCallToActionBackgroundColor(ColorDrawable callToActionBackgroundColor) {
       this.styles.callToActionBackgroundColor = callToActionBackgroundColor;
       return this;
     }
 
+    @CanIgnoreReturnValue
     public Builder withPrimaryTextTypeface(Typeface primaryTextTypeface) {
       this.styles.primaryTextTypeface = primaryTextTypeface;
       return this;
     }
 
+    @CanIgnoreReturnValue
     public Builder withPrimaryTextSize(float primaryTextSize) {
       this.styles.primaryTextSize = primaryTextSize;
       return this;
     }
 
+    @CanIgnoreReturnValue
     public Builder withPrimaryTextTypefaceColor(int primaryTextTypefaceColor) {
       this.styles.primaryTextTypefaceColor = primaryTextTypefaceColor;
       return this;
     }
 
+    @CanIgnoreReturnValue
     public Builder withPrimaryTextBackgroundColor(ColorDrawable primaryTextBackgroundColor) {
       this.styles.primaryTextBackgroundColor = primaryTextBackgroundColor;
       return this;
     }
 
+    @CanIgnoreReturnValue
     public Builder withSecondaryTextTypeface(Typeface secondaryTextTypeface) {
       this.styles.secondaryTextTypeface = secondaryTextTypeface;
       return this;
     }
 
+    @CanIgnoreReturnValue
     public Builder withSecondaryTextSize(float secondaryTextSize) {
       this.styles.secondaryTextSize = secondaryTextSize;
       return this;
     }
 
+    @CanIgnoreReturnValue
     public Builder withSecondaryTextTypefaceColor(int secondaryTextTypefaceColor) {
       this.styles.secondaryTextTypefaceColor = secondaryTextTypefaceColor;
       return this;
     }
 
+    @CanIgnoreReturnValue
     public Builder withSecondaryTextBackgroundColor(ColorDrawable secondaryTextBackgroundColor) {
       this.styles.secondaryTextBackgroundColor = secondaryTextBackgroundColor;
       return this;
     }
 
+    @CanIgnoreReturnValue
     public Builder withTertiaryTextTypeface(Typeface tertiaryTextTypeface) {
       this.styles.tertiaryTextTypeface = tertiaryTextTypeface;
       return this;
     }
 
+    @CanIgnoreReturnValue
     public Builder withTertiaryTextSize(float tertiaryTextSize) {
       this.styles.tertiaryTextSize = tertiaryTextSize;
       return this;
     }
 
+    @CanIgnoreReturnValue
     public Builder withTertiaryTextTypefaceColor(int tertiaryTextTypefaceColor) {
       this.styles.tertiaryTextTypefaceColor = tertiaryTextTypefaceColor;
       return this;
     }
 
+    @CanIgnoreReturnValue
     public Builder withTertiaryTextBackgroundColor(ColorDrawable tertiaryTextBackgroundColor) {
       this.styles.tertiaryTextBackgroundColor = tertiaryTextBackgroundColor;
       return this;
     }
 
+    @CanIgnoreReturnValue
     public Builder withMainBackgroundColor(ColorDrawable mainBackgroundColor) {
       this.styles.mainBackgroundColor = mainBackgroundColor;
       return this;
